81228c9bf8e31b057d5f12d32e36ca775a854c04
KRTirtho <krtirtho@gmail.com>
2021-06-07 23:47:36 +0600

    Configured scripts local development env setup done

---
+++ vite.config.react.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+/**
+ * @type {import('vite').UserConfig}
+*/
+export default defineConfig({
+  plugins: [reactRefresh()],
+  root: "src/react/local"
+})
